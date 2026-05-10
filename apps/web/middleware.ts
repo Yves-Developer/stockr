975be057863a8ba632845185b6b62004386d78da
Yves-Developer <yvesmugisha09@gmail.com>
2026-05-10 15:58:40 +0200

    fix: protect /scanner route with middleware to ensure mobile users are logged in

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -21,5 +21,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/scanner/:path*", "/scanner"],
 };
